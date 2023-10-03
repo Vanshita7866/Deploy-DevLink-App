0f0e059235b9068a914936486a639e99fecc3e61
Vanshita7866 <128288729+Vanshita7866@users.noreply.github.com>
2023-10-03 09:53:26 +0530

    Update slideshow.jsx

--- src/slideshow.jsx
+++ src/slideshow.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
+
 const images = [
-  "https://picsum.photos/id/102/4320/3240",
-  "https://picsum.photos/id/10/4320/3240",
-  "https://picsum.photos/id/17/4320/3240",
-  "https://picsum.photos/id/107/4320/3240",
-  "https://picsum.photos/id/104/4320/3240",
-  "https://picsum.photos/id/19/4320/3240",
-  "https://picsum.photos/id/167/4320/3240",
-  "https://picsum.photos/id/56/4320/3240",
-  "https://picsum.photos/id/34/4320/3240",
-  "https://picsum.photos/id/77/4320/3240",
-  "https://picsum.photos/id/44/4320/3240",
-  "https://picsum.photos/id/27/4320/3240",
-];
+    "https://picsum.photos/id/102/4320/3240",
+    "https://picsum.photos/id/10/4320/3240",
+    "https://picsum.photos/id/17/4320/3240",
+    "https://picsum.photos/id/107/4320/3240",
+    "https://picsum.photos/id/104/4320/3240",
+    "https://picsum.photos/id/19/4320/3240",
+    "https://picsum.photos/id/167/4320/3240",
+    "https://picsum.photos/id/56/4320/3240",
+    "https://picsum.photos/id/34/4320/3240",
+    "https://picsum.photos/id/77/4320/3240",
+    "https://picsum.photos/id/44/4320/3240",
+    "https://picsum.photos/id/27/4320/3240",
+  ];
 
 const Slideshow = () => {
   const [slideIndex, setSlideIndex] = useState(1);
@@ -25,39 +26,39 @@ const Slideshow = () => {
     setSlideIndex(n);
   };
 
-  const showSlides = (n) => {
-    let i
-    const slides = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-      setSlideIndex(1);
-    }
-    if (n < 1) {
-      setSlideIndex(slides.length);
-    }
-
-    for (i = 0; i < slides.length; i++) {
-      (slides[i]).style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    (slides[slideIndex - 1]).style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  };
-  
   useEffect(() => {
+    const showSlides = (n) => {
+      let i;
+      const slides = document.getElementsByClassName("mySlides");
+      const dots = document.getElementsByClassName("dot");
+
+      if (n > slides.length) {
+        setSlideIndex(1);
+      }
+      if (n < 1) {
+        setSlideIndex(slides.length);
+      }
+
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+    };
+
     showSlides(slideIndex);
-  }, [slideIndex , showSlides]);
+  }, [slideIndex]);
 
   return (
     <div>
       <div className="slideshow-container">
         {images.map((image, index) => (
           <div className="mySlides fade" key={index}>
-            <img src={image} style={{ height: "300px", width: "100%" }} alt ={"slideshow images"}/>
+            <img src={image} style={{ height: "300px", width: "100%" }} alt="slideshow images" />
           </div>
         ))}
 
