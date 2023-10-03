bc0797bd1b15abd567c24018fc8c9c4b60371db8
Vanshita7866 <128288729+Vanshita7866@users.noreply.github.com>
2023-10-03 09:14:01 +0530

    Update customer.jsx

--- src/customer.jsx
+++ src/customer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Card from "./card";
 import customerlist from "./customerlist";
-import { Button } from "semantic-ui-react";
+//import { Button } from "semantic-ui-react";
 import "./card.css";
 
 
