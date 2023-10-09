d781f166ddb1e937c354a5a17bdba9bc421afb1f
Vanshita7866 <128288729+Vanshita7866@users.noreply.github.com>
2023-10-09 05:34:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-//import { useState } from "react";
+import { useState } from "react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 import Header from "./Header";
 import HomePage from './Homepage'
 import Login from './Login'
 import Signup from "./Signup";
-import { Routes, Route } from'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './context/user.context';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+     <UserProvider>
+     <Routes>
         <Route element={<Header />} />
         <Route index element={<HomePage />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Signup' element={<Signup />} />
       </Routes>
-    </BrowserRouter>
+
+     </UserProvider>
+     </BrowserRouter>
   );
 }
 
